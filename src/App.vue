7c05b77f9802a8707070639a7d413ed07bee99be
Ed <silva.email@yahoo.com>
2024-03-24 06:46:55 -0500

    Refactor App.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <h3>Hey!</h3>
-  <greeting :age="age"></greeting>
-  <user :age="age" @age-change="updateAge"></user>
+<h3>Hey!</h3>
+<greeting :age="age"></greeting>
+<user :age="age" @age-change="updateAge"></user>
 </template>
 
 <script>
 import Greeting from './components/Greeting.vue';
 import User from './components/User.vue'
 export default {
-  name: "App",
-  components: {
-    Greeting,
-    User
-  },
-  data() {
-    return {
-      age: 20
+    name: "App",
+    components: {
+        Greeting,
+        User
+    },
+    data() {
+        return {
+            age: 20
+        }
+    },
+    methods: {
+        updateAge(num) {
+            this.age += num
+        }
     }
-  },
-  methods: {
-    updateAge(num) {
-      this.age += num
-    }
-  }
 }
 </script>./components/Greeting.vue
